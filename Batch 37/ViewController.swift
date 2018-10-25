8090c0c75ee6bf21a93a29b6ab15b203e9dbe3da
Rio Rinaldi <riorinaldi@rios-macbook-pro.local>
2018-10-25 09:49:51 +0700

    Initial Commit

---
+++ Batch 37/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Batch 37
+//
+//  Created by Rio Rinaldi on 25/10/18.
+//  Copyright © 2018 Rio Rinaldi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Batch 37Tests/Batch_37Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Batch_37Tests.swift
+//  Batch 37Tests
+//
+//  Created by Rio Rinaldi on 25/10/18.
+//  Copyright © 2018 Rio Rinaldi. All rights reserved.
+//
+
+import XCTest
+@testable import Batch_37
+
+class Batch_37Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
